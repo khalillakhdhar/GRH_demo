0dc4365aebbe512053c5a5db8c7340bcbd812489
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-25 20:00:02 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,29 @@
 <body>
     <?php include("header.php"); ?>
     <br><br>
+    <!-- Button trigger modal -->
+    <div align="center"> <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+            data-bs-target="#exampleModal">
+            Ajoutez un employé
+        </button>
+    </div>
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ajouter</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
+                </div>
+            </div>
+        </div>
+    </div>
     <table class="table table-bordered">
         <thead>
             <tr>
