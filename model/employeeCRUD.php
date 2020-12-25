28c25edf3c7bdf4badd03adff11d5cfd349d61e6
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-25 20:21:25 +0100

    Update employeeCRUD.php

--- model/employeeCRUD.php
+++ model/employeeCRUD.php
@@ -1,1 +1,23 @@
 <?php
+class Employee
+{
+    function list_users()
+    {
+        //bloc de connexion
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "web_mp";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        // lecture
+        $sql = "SELECT * FROM employee";
+        $result = $conn->query($sql); //chargement dans result
+        return $result; //retour
+    }
+}
