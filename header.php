5103c27d859e9439024b5eaec9954022aa892212
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-25 19:42:15 +0100

    Update header.php

--- header.php
+++ header.php
@@ -10,6 +10,27 @@
     </script>
 </head>
 
-<body></body>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">GRH</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Employées</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Congés</a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+</body>
 
 </html>
