fc09b50f5b04e1e69ffaa2cb7d814e864c4ada0f
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-25 19:53:41 +0100

    table

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="index.php">Employées</a>
+                        <a class="nav-link active" aria-current="page" href="index.php">Employés</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="conges.php">Congés</a>
--- index.php
+++ index.php
@@ -9,5 +9,31 @@
 
 
 </body>
+<table class="table table-bordered">
+    <thead>
+        <tr>
+            <th>Nom</th>
+            <th>Téléphone</th>
+            <th>adresse</th>
+            <th>Grade</th>
+            <th>Salaire</th>
+            <th>CIN</th>
+            <th>Action</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Nom</td>
+            <td>Téléphone</td>
+            <td>adresse</td>
+            <td>Grade</td>
+            <td>Salaire</td>
+            <th>CIN</th>
+            <td><a href="#">supprimer</a></td>
+        </tr>
+
+    </tbody>
+
+</table>
 
 </html>
