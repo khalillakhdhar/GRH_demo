76292efd580066edecb89d6c497e9b9ad9571fcc
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-25 19:55:23 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,34 +6,34 @@
 
 <body>
     <?php include("header.php"); ?>
+    <br><br>
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>Nom</th>
+                <th>Téléphone</th>
+                <th>adresse</th>
+                <th>Grade</th>
+                <th>Salaire</th>
+                <th>CIN</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Nom</td>
+                <td>Téléphone</td>
+                <td>adresse</td>
+                <td>Grade</td>
+                <td>Salaire</td>
+                <th>CIN</th>
+                <td><a href="#">supprimer</a></td>
+            </tr>
 
+        </tbody>
+
+    </table>
 
 </body>
-<table class="table table-bordered">
-    <thead>
-        <tr>
-            <th>Nom</th>
-            <th>Téléphone</th>
-            <th>adresse</th>
-            <th>Grade</th>
-            <th>Salaire</th>
-            <th>CIN</th>
-            <th>Action</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>Nom</td>
-            <td>Téléphone</td>
-            <td>adresse</td>
-            <td>Grade</td>
-            <td>Salaire</td>
-            <th>CIN</th>
-            <td><a href="#">supprimer</a></td>
-        </tr>
-
-    </tbody>
-
-</table>
 
 </html>
